7a20cb5d3227a4bea565c89c8382eb0d3896526a
Bazil001 <bazilansari042@gmail.com>
2024-09-22 21:13:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7533492997:AAFmQpHL7SgMEJKcghCX-Z8ySh4Npzoqx3M")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("GoTo_HellxD")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5575457497")
+
+
+
+
+
+  
